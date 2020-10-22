df52d07fa48b20402dae38c2a0124ed37116fe3d
WebnicAlex <aleks-batka@yandex.ua>
2020-10-22 23:58:14 +0300

    Module 3. All task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -5,5 +5,13 @@
  * @returns {string}
  */
 function showSalary(users, age) {
-  // ваш код...
+  let finalStr = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].age <= age) {
+      finalStr += `${arr[i].name}, ${arr[i].balance}\n`;
+    };
+  };
+
+  return finalStr = `'${finalStr.slice(0, finalStr.length - 1)}'`;
 }
