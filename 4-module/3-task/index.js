b5b4a9152f799ca162271a812faf982762be2075
WebnicAlex <aleks-batka@yandex.ua>
2020-10-30 18:16:58 +0200

    Module 4. Final

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,42 +2,35 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
-function highlight(table) {
 
-  let rows = table.tBodies[0].rows;
 
-      for (let tr of rows) {
-        let tds = tr.cells;
+    function highlight(table) {
+      let rows = table.rows;
 
-        for (let td of tds) {
+      for (let i = 0; i < rows.length; i++) {
+        let row = rows[i].cells;
 
-          if (tds[1]) {
-            if (+tds[1].innerHTML < 18) {
-              tds[1].parentElement.style.textDecoration = 'line-through';
-            }
+        for (let j = 0; j < row.length; j++) {
+          let cell = row[j];
+
+          if (+cell.innerHTML < 18) {
+            cell.parentElement.style.textDecoration = 'line-through';
           }
 
-          if (tds[2]) {
-            if (tds[2].innerHTML == 'm') {
-              tds[2].parentElement.className = 'male';
-            }
-            if (tds[2].innerHTML == 'f') {
-              tds[2].parentElement.className = 'female';
-            }
+          if (cell.innerHTML == 'm') {
+            cell.parentElement.classList.add('male');
+          }
+          if (cell.innerHTML == 'f') {
+            cell.parentElement.classList.add('female');
           }
 
-          if (tds[3]) {
-            if (tds[3].matches('[data-available="true"]')) {
-              tds[3].parentElement.classList.add('available')
-            } else if (tds[3].matches('[data-available="false"]')) {
-              tds[3].parentElement.classList.add('unavailable');
-            } else {
-              tds[3].parentElement.hidden = true;
-            }
+          if (j == (row.length - 1) && cell.parentElement.parentElement.tagName !== 'THEAD') {
+            if (cell.matches('[data-available="true"]')) {
+              cell.parentElement.classList.add('available')
+            } else if (cell.matches('[data-available="false"]')) {
+              cell.parentElement.classList.add('unavailable');
+            } else (cell.parentElement.hidden = true);
           }
         }
       }
-
-
-}
-// td.cellIndex
+    };
