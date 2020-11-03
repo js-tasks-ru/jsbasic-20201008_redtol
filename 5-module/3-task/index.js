703040ef1369f9c84a0c886bf7c3f572227b8a91
WebnicAlex <aleks-batka@yandex.ua>
2020-11-03 18:21:44 +0200

    Module 5. All task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+    let arrLeft = document.querySelector('.carousel__arrow_left');
+    let arrRight = document.querySelector('.carousel__arrow_right');
+    let slidersWrap = document.querySelector('.carousel__inner');
+    let slide = document.querySelector('.carousel__slide');
+    let slideWidth = slide.offsetWidth;
+    let sliderPosition = 0;
+    let totalSlider = document.querySelectorAll('.carousel__slide');
+
+    arrLeft.style.display = 'none';
+    arrLeft.addEventListener("click", function() {
+        sliderPosition += slideWidth;
+        slidersWrap.style.transform = `translateX(${sliderPosition}px)`;
+        if (!sliderPosition) {
+            arrLeft.style.display = 'none';
+        }
+        if (sliderPosition > -(slideWidth * (totalSlider.length - 1))) {
+            arrRight.style.display = '';
+        }
+    });
+    arrRight.addEventListener("click", function() {
+        sliderPosition -= slideWidth;
+        slidersWrap.style.transform = `translateX(${sliderPosition}px)`;
+        if (sliderPosition) {
+            arrLeft.style.display = '';
+        }
+        if (sliderPosition < -(slideWidth * (totalSlider.length - 2))) {
+            arrRight.style.display = 'none';
+        }
+    });
+
 }
